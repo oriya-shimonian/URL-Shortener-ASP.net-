c5fc8ac1366a5e1f41175dabcb75b75189554fa0
oriya <oriyashimonian@gmail.com>
2025-09-26 16:36:58 +0300

    fix: remove uneccessary code

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -1,37 +1,3 @@
-// using UrlShortener.Api.Infrastructure;
-// using UrlShortener.Api.Services;
-// using FluentValidation;
-// using UrlShortener.Api.Application.Validators;
-
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Services
-// builder.Services.AddInfrastructure(builder.Configuration);
-// builder.Services.AddControllers();
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-// builder.Services.AddScoped<ShortUrlService>();
-// builder.Services.AddHttpContextAccessor();
-// builder.Services.AddValidatorsFromAssemblyContaining<CreateShortUrlRequestValidator>();
-
-// var app = builder.Build();
-
-// // Pipeline
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-// app.MapControllers();
-
-// var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-// app.Urls.Add($"http://0.0.0.0:{port}");
-
-// app.Run();
-
-
 using UrlShortener.Api.Infrastructure;
 using UrlShortener.Api.Services;
 using FluentValidation;
